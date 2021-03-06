2fff825903b95f66005cc2d3dd5714bf0af1e5de
debjeanlee <debsgenes@gmail.com>
2021-03-06 16:43:22 +0800

    add wallet icon to header

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,11 +1,23 @@
 <template>
     <div class="header">
-        <h1>No Money, Why?</h1>
+        <h1>
+            <FontAwesomeIcon class="wallet" :icon="myIcon" />
+            No Money, <em>Why?</em>
+        </h1>
     </div>
 </template>
 
 <script>
+    import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+    import { faWallet } from '@fortawesome/free-solid-svg-icons'
 
+    export default {
+        components: { FontAwesomeIcon },
+        setup(){
+            const myIcon = faWallet
+            return { myIcon }
+        },
+    }
 </script>
 
 <style lang="scss">
@@ -13,5 +25,8 @@
     text-align: left;
     background-color: $lgreen;
     padding: 1em 2em;
+}
+.wallet {
+    margin-right: 0.2em;
 }
 </style>
