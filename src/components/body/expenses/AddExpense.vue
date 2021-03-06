9436b9a521f5db4d83d00b1075bb05cc82aa1bfd
debjeanlee <debsgenes@gmail.com>
2021-03-07 00:15:48 +0800

    form styling

--- src/components/body/expenses/AddExpense.vue
+++ src/components/body/expenses/AddExpense.vue
@@ -1,18 +1,28 @@
 <template>
-    <div>
-        <div>
-            <div class="dropdown" @click="toggleOpen" :ref="selectedCategory">
-                <div class="flex between align-center">
-                    <div>{{ selectedCategory }}</div>
-                    <FontAwesomeIcon :icon="arrow" />
+    <div class="flex center">
+        <form action="">
+            <!-- DESCRIPTION -->
+            <div class="form-label">Description: </div>
+            <input type="text" v-model="description" placeholder="potato..">
+            <!-- DATE -->
+            <div class="form-label">Date: </div>
+            <input type="date" v-model="date" class="date-picker" >
+            <!-- CATEGORY DROPDOWN -->
+            <div class="form-label">Category: </div>
+            <div>
+                <div class="dropdown" @click="toggleOpen">
+                    <div class="flex between align-center">
+                        <div>{{ selectedCategory }}</div>
+                        <FontAwesomeIcon :icon="arrow" />
+                    </div>
+                </div>
+                <div v-if="openSelect" class="options-wrapper">
+                    <li class="option" v-for="category in categories" @click="handleSelect(category)" :key="category">
+                        {{ category }}
+                    </li>
                 </div>
             </div>
-            <div v-if="openSelect" class="options-wrapper">
-                <li class="option" v-for="category in categories" @click="handleSelect(category)" :key="category">
-                    {{ category }}
-                </li>
-            </div>
-        </div>
+        </form>
     </div>
 </template>
 
@@ -25,10 +35,12 @@ import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
     export default {
         components: { FontAwesomeIcon },
         setup(){
+            const { categories } = getCategories();
+            const description = ref('')
+            const date = ref(null)
             const arrow = faChevronDown
             const openSelect = ref(false)
             const selectedCategory = ref('Food');
-            const { categories } = getCategories();
             
             const toggleOpen = () => {
                 return openSelect.value = !openSelect.value
@@ -39,11 +51,25 @@ import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
                 toggleOpen();
             }
 
-            return { categories, selectedCategory, openSelect, toggleOpen, handleSelect, arrow }
+            return { 
+                categories, 
+                selectedCategory,
+                openSelect, 
+                toggleOpen, 
+                handleSelect, 
+                arrow,
+                date,
+                description
+            }
         }
     }
 </script>
 
 <style lang="scss">
+@media screen and (max-width: 768px) {
+  .date-picker {
+      background-color: $white;
+  }
+}
 
 </style>
