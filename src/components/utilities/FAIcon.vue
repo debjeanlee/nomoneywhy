e316b5b6d1e8843a1986d418e42a857fc7f301c5
debjeanlee <debsgenes@gmail.com>
2021-03-06 16:30:12 +0800

    FA Icon eg added

---
+++ src/components/utilities/FAIcon.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <font-awesome-icon :icon="myIcon" />
+  </div>
+</template>
+
+<script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+
+export default {
+    components: { FontAwesomeIcon },
+    setup(){
+        const myIcon = faSpinner
+        return { myIcon }
+    },
+}
+</script>
